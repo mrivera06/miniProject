98ac2f46b9fb3826284d6a44e58ef4cf957b3b67
mrivera06 <mariely.rivera@patelhs.org>
2021-01-29 17:23:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) 
+  {
+
+    System.out.println(zodiac(zodiacSign));
+    System.out.println(birthday(birthdayFun));
+  }
+    //the zodiac asks for what their star sign is
+    static String zodiac(String zodiacSign)
+    {
+      String sign = "Hey there, what is your zodiac sign? " + zodiacSign;
+      return sign;
+    }
+     //birthday is a if/else statement that asks if it is raining
+     static boolean birthday(boolean birthdayFun)
+    {
+      boolean fun =  "Is it raining outside? T/F";
+      return fun;
+      {
+        if (true)
+        {
+        System.out.println("I'm sorry you can't go out right now maybe later");
+        }
+      }
+      else (false)
+      {
+        System.out.println("Lets go outside and have fun!");
+      }
+
+    }
+
+  
+}
